2f82eae7ca08f61c2d6d1767cdf012d6f9f38d48
orsolon <56239735+orsolon@users.noreply.github.com>
2021-02-18 20:29:41 -0300

    fix

--- src/app/perfil/perfil.service.ts
+++ src/app/perfil/perfil.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Postagem } from './perfil.interface';
+import { Postagem, Usuario } from './perfil.interface';
 
 
 @Injectable({
@@ -8,18 +8,16 @@ import { Postagem } from './perfil.interface';
 })
 export class PerfilService {
 
-  
-
   constructor(private http: HttpClient) { }
 
   // consultar api
-  public GetPerfilUsuario(): Array<String> {
+  public GetPerfilUsuario() {
     
     return this.http.get<Usuario>('');
   }
 
   public GetPostagens(){
-    return this.http.get<Postagem>('');
+    return this.http.get<Postagem[]>('');
   }
   
 
