665eabedee0a22b2620eca0102bdcd733897d9e8
Pri-Alves <priscillarafaela19@gmail.com>
2021-02-20 18:40:42 -0300

    adicionando novo usuario

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     CommonModule,
     LoginRoutingModule,
     FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
